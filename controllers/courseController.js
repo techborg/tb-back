fa17c454c1d2768edf73fe510b03a7f2bcd113c3
kimbohlovette <kimbohlovette@gmail.com>
2025-07-10 17:58:31 +0100

    fixed unresolved imports in courses and auth controllers

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,57 +1,58 @@
-const Course = require('../models/Course');
+const Course = require("../models/course");
 
 // Create a new course
 exports.createCourse = async (req, res) => {
-  try {
-    const course = new Course(req.body);
-    await course.save();
-    res.status(201).json(course);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+	try {
+		const course = new Course(req.body);
+		await course.save();
+		res.status(201).json(course);
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
 };
 
 // Get all courses
 exports.getAllCourses = async (req, res) => {
-  try {
-    const courses = await Course.find();
-    res.json(courses);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+	try {
+		const courses = await Course.find();
+		res.json(courses);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 };
 
 // Get a single course
 exports.getCourseById = async (req, res) => {
-  try {
-    const course = await Course.findById(req.params.id);
-    if (!course) return res.status(404).json({ message: 'Course not found' });
-    res.json(course);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+	try {
+		const course = await Course.findById(req.params.id);
+		if (!course)
+			return res.status(404).json({ message: "Course not found" });
+		res.json(course);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 };
 
 // Delete a course
 exports.deleteCourse = async (req, res) => {
-  try {
-    await Course.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Course deleted' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+	try {
+		await Course.findByIdAndDelete(req.params.id);
+		res.json({ message: "Course deleted" });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 };
 // Update a course
 exports.updateCourse = async (req, res) => {
-  try {
-    const course = await Course.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true, runValidators: true }
-    );
-    if (!course) return res.status(404).json({ message: 'Course not found' });
-    res.json(course);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+	try {
+		const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+		if (!course)
+			return res.status(404).json({ message: "Course not found" });
+		res.json(course);
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
 };
